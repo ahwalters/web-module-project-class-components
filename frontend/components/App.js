cdfc6b224a149af2c96c0a74d83a505a51a3a6c1
Alana Walters <lana@Alanas-MacBook-Air.local>
2023-03-25 21:54:52 -0400

    created subcomponents

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,50 @@
 import React from 'react'
+import TodoList from './TodoList';
 
 export default class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {todos : 
+      [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          name: 'Finish Unit',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          name: 'Practice Spanish',
+          id: 1528817084358,
+          completed: true
+        }
+      ]
+    }
+  }
   render() {
+    const {todos} = this.state;
+
     return (
       <div>
-        Todo App
+        <h2>To Dos:</h2>
+
+        <TodoList todos={todos}/>
+
+        <form>
+          <input />
+          <button></button>
+        </form>
+
+        <button></button>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -4,7 +4,12 @@ export default class Form extends React.Component {
   render() {
     return (
       <div>
-        Form
+        <form>
+          <input />
+          <button></button>
+        </form>
+
+        <button></button>
       </div>
     )
   }
